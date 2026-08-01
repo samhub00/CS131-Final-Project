e8f5749f888f697e09a4061b296211e624014d75
KyleJamesSJSU <kyle.james@sjsu.edu>
2026-08-01 13:48:44 -0700

    added custom schema to ml_textlength.py, more tests

--- 3_scaling/ml_textlength.py
+++ 3_scaling/ml_textlength.py
@@ -2,19 +2,50 @@ import sys
 
 from pyspark.sql import SparkSession, functions as F
 from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, FloatType
 
 input_path = sys.argv[1]
 #output_path = sys.argv[2]
 
 spark = SparkSession.builder.appName("top15reviewedgames").getOrCreate() 
 
+# define custom schema
+schema = StructType([
+    StructField('recommendationid', LongType(), True),
+    StructField('appid', IntegerType(), True),
+    StructField('game', StringType(), True),
+    StructField('author_steamid', LongType(), True),
+    StructField('author_num_games_owned', IntegerType(), True),
+    StructField('author_num_reviews', IntegerType(), True),
+    StructField('author_playtime_forever', LongType(), True),
+    StructField('author_playtime_last_two_weeks', IntegerType(), True),
+    StructField('author_playtime_at_review', IntegerType(), True),
+    StructField('author_last_played', IntegerType(), True),
+    StructField('language', StringType(), True),
+    StructField('review', StringType(), True),
+    StructField('timestamp_created', LongType(), True),
+    StructField('timestamp_updated', LongType(), True),
+    StructField('voted_up', IntegerType(), True),
+    StructField('votes_up', IntegerType(), True),
+    StructField('votes_funny', IntegerType(), True),
+    StructField('weighted_vote_score', FloatType(), True),
+    StructField('comment_count', IntegerType(), True),
+    StructField('steam_purchase', IntegerType(), True),
+    StructField('received_for_free', IntegerType(), True),
+    StructField('written_during_early_access', IntegerType(), True),
+    StructField('hidden_in_steam_china', IntegerType(), True),
+    StructField('steam_china_location', StringType(), True),
+])
+
 df = (spark.read.format("csv")
-	.option("inferSchema", "true")
+	.schema(schema)
 	.option("header", "true")
 	.load(input_path))
 
 #df.show(5)
 #df.printSchema()
+# check that schema worked
+df.show(25)
 
 # select game name column
 # count occurrences of each game name
@@ -23,9 +54,40 @@ df = (spark.read.format("csv")
 #df.select("game").groupBy("game").count().orderBy("count", ascending=False).show(15)
 
 # filter data first
-# get review column and review status (review, voted_up)
-s1 = df.select("review", "voted_up")
+# need to view the voted_up column to actually determine what it is, had some odd values in it
+#df.select("voted_up").groupBy("voted_up").count().orderBy("count", ascending=False).show(25)
+# +--------------------+--------+
+# |            voted_up|   count|
+# +--------------------+--------+
+# |                   1|95383123|
+# |                   0|15545068|
+# |                NULL|   25767|
+# | and I became the...|    3061|
+# | если остался про...|    2464|
+# |        ~: : : : : :|    1741|
+# |             however|    1641|
+# ...
+# 1 seems to be positive and 0 seems negative. there's also NULL and other junk data from the review in there
+# choosing to exclude rows with a voted_up value that isn't 1 or 0
+# may as well split the data into positive and negative reviews
+positive_reviews = df.select("review","voted_up").filter("voted_up == 1")
+negative_reviews = df.select("review","voted_up").filter("voted_up == 0")
+
+# show to check
+#positive_reviews.show(10)
+#negative_reviews.show(10)
+
+
+
+
 # convert review column into string length of column
-s1.withColumn("review_length", F.length("review")).show(15)
+pos_review_length = positive_reviews.withColumn("review_length", F.length("review")).select("review_length","voted_up")
+pos_review_length.show(10)
+
+neg_review_length = negative_reviews.withColumn("review_length", F.length("review")).select("review_length","voted_up")
+neg_review_length.show(10)
+
+# drop the review text because there are slurs :(
+
 
 spark.stop()
